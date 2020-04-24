6839d93fab51cca2a89df226bcdf14a192bda73b
Elnar Ibragimov <djelnar@gmail.com>
2020-04-24 07:47:09 +0500

    Edit

--- Makefile
+++
@@ -1,2 +0,0 @@
-store.zip:
-	zip store.zip LICENSE README.md background.js icon-*.png manifest.json
--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
  * Pattern matching the prefix of at least one stripped query string
  * parameter. We'll search the query string portion of the URL for this
@@ -10,7 +12,7 @@ var searchPattern = new RegExp("utm_|clid|_hs|icid|igshid|mc_|mkt_tok", "i");
  * stripped from the final URL.
  */
 var replacePattern = new RegExp(
-  "([?&]" +
+  "([?&#]" +
     "(icid|mkt_tok|(g|fb)clid|igshid|_hs(enc|mi)|mc_[ce]id|utm_(source|medium|term|campaign|content|cid|reader|referrer|name|social|social-type))" +
     "=[^&#]*)",
   "ig"
@@ -19,20 +21,40 @@ var replacePattern = new RegExp(
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     var url = details.url;
+
     var queryStringIndex = url.indexOf("?");
+
+    var stripped = url;
+
     if (url.search(searchPattern) > queryStringIndex) {
-      var stripped = url.replace(replacePattern, "");
+      stripped = url.replace(replacePattern, "");
       if (stripped.charAt(queryStringIndex) === "&") {
         stripped =
           stripped.substr(0, queryStringIndex) +
           "?" +
           stripped.substr(queryStringIndex + 1);
       }
-      if (stripped != url) {
-        return { redirectUrl: stripped };
+    }
+
+    var hashStringIndex = stripped.indexOf("#");
+
+    if (stripped.search(searchPattern) > hashStringIndex) {
+      stripped = stripped.replace(replacePattern, "");
+      if (stripped.charAt(hashStringIndex) === "&") {
+        stripped =
+          stripped.substr(0, hashStringIndex) +
+          "#" +
+          stripped.substr(hashStringIndex + 1);
       }
     }
+
+    if (stripped !== url) {
+      return { redirectUrl: stripped };
+    }
   },
-  { urls: ["https://*/*?*", "http://*/*?*"], types: ["main_frame"] },
+  {
+    urls: ["<all_urls>"],
+    types: ["main_frame"],
+  },
   ["blocking"]
 );
